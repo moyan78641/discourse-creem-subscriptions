17fc94c2926efd2892b73eb691c56fb1337f3370
Orion <131159175+moyan78641@users.noreply.github.com>
2026-01-15 19:42:02 +0800

    Delete lib/creem_subscriptions directory

--- lib/creem_subscriptions/engine.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module ::CreemSubscriptions
-  class Engine < ::Rails::Engine
-    engine_name "discourse-creem-subscriptions"
-    isolate_namespace CreemSubscriptions
-  end
-end
